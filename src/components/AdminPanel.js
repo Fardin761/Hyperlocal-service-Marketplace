a0f58c43a6be2e1525811fcb47f40faa99e5860c
Fardin761 <fk8068668@gmail.com>
2025-07-21 22:09:59 +0530

    Delete src directory

--- src/components/AdminPanel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function AdminPanel() {
-  return (
-    <div className="container-card">
-      <h2>Admin Panel</h2>
-      <p>Manage providers, users, and services.</p>
-    </div>
-  );
-}
